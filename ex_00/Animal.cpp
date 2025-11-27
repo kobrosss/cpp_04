42fc94ec5cfe6e2bcc9b00ccfeade17358fedffe
Rostyslav Kobeliev <rostyslavkobeliev@gmail.com>
2025-11-27 19:24:49 +0100

    ex00, almost done

---
+++ ex_00/Animal.cpp
@@ -0,0 +1,35 @@
+#include "Animal.hpp"
+
+Animal::Animal() : type ("Animal")
+{
+	std::cout << "Animal: Default constructor" << std::endl;
+}
+
+Animal::Animal(const Animal &other)
+{
+	std::cout << "Animal: Copy constructor" << std::endl;
+}
+
+Animal& Animal::operator=(const Animal &other)
+{
+	std::cout << "Animal: & operator constructor" << std::endl;
+	if(this != &other)
+		type = other.type;
+	return *this;
+}
+
+Animal::~Animal()
+{
+	std::cout << "Animal: Destructor" << std::endl;
+}
+
+std::string Animal::getType() const
+{
+	return type;
+}
+
+void Animal::makeSound() const
+{
+	std::cout << "Animal: *generic animal sound*" << std::endl;
+}
+
---
+++ ex_00/Cat.cpp
@@ -0,0 +1,30 @@
+#include "Cat.hpp"
+
+Cat::Cat()
+{
+	type = "Cat";
+	std::cout << "Cat: Default constructor" << std::endl;
+}
+
+Cat::Cat(const Cat &other)
+{
+	std::cout << "Cat: Copy constructor" << std::endl;
+}
+
+Cat &Cat::operator=(const Cat &other)
+{
+	std::cout << "Cat: Copy assignment " << std::endl;
+	if (this != &other)
+		Animal::operator=(other);
+	return *this;
+}
+
+Cat::~Cat()
+{
+	std::cout << "Cat: Destructor" << std::endl;
+}
+
+void Cat::makeSound() const
+{
+	std::cout << "Cat: Meow! Meow!" << std::endl;
+}
